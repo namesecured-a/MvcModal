29820678f0f898499508537f1c439409b1eb0a4d
Sergey Psekha <pss@ciklum.com>
2014-02-05 16:46:24 +0200

    master: added solution;

---
+++ Sources/MvcModal/Models/MvcModelDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcModal.Models
+{
+    public class MvcModelDbContext : DbContext
+    {
+        private const string ConnectionStringName = "MvcModelContext";
+
+        public MvcModelDbContext()
+            : base(ConnectionStringName)
+        {
+        }
+
+        public IDbSet<User> Users { get; set; }
+
+        public virtual void Commit()
+        {
+            this.SaveChanges();
+        }
+    }
+}
