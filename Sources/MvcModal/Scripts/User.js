33d5b8d4b389c0749574080cdbd9fc94f5c56c85
Sergey Psekha <pss@ciklum.com>
2014-02-07 14:31:51 +0200

    fixed "Twitter bootstrap remote modal shows same content everytime"
    http://stackoverflow.com/questions/12286332/twitter-bootstrap-remote-modal-shows-same-content-everytime

--- Sources/MvcModal/Scripts/User.js
+++ Sources/MvcModal/Scripts/User.js
@@ -7,9 +7,15 @@ MvcModal.User = MvcModal.User || (function () {
             e.preventDefault();
 
             $(params.dialogFormSelector).on('loaded.bs.modal', function() {
-                $(params.submitButtonSelector).on('click', function() {
+                $(params.submitButtonSelector).on('click', function () {
                     $(params.dialogFormSelector).modal('hide');
                 });
+            });
+
+            // Twitter bootstrap remote modal shows same content everytime
+            // http://stackoverflow.com/questions/12286332/twitter-bootstrap-remote-modal-shows-same-content-everytime
+            $(params.dialogFormSelector).on('hidden.bs.modal', function () {
+                $(this).removeData('bs.modal');
             });
 
             $(params.dialogFormSelector).modal({
