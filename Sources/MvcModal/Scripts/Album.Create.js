89532537d04298b227a8bc0ebdf7003c089581ae
Sergey Psekha <pss@ciklum.com>
2014-02-09 17:54:26 +0200

    refactored;

--- Sources/MvcModal/Scripts/Album.Create.js
+++ Sources/MvcModal/Scripts/Album.Create.js
@@ -1,44 +1,20 @@
 ﻿MvcModal = window.MvcModal || {};
-MvcModal.Album.Create = MvcModal.Album.Create || (function () {
-    var self = this;
+MvcModal.Album = MvcModal.Album || {};
 
-    this.getInstance = function (parameters) {
-        var params = parameters;
-        
-        var instance = {
+MvcModal.Album.Create = MvcModal.Album.Create || (function() {
+    var getInstance = function(parameters) {
+        return {
             Id: 0,
             Artist: 'Long name',
             Genre: 'Chillout'
         };
-
-        return instance;
-    };
-
-    this.submit = function (parameters) {
-        var url = parameters.url;
-        var instance = parameters.instance;
-        var success = parameters.success;
-        var error = parameters.error;
-        var json = JSON.stringify(instance);
-        $.ajax({
-            type: 'POST',
-            url: url,
-            datatype: 'json',
-            contentType: 'application/json: charset=utf-8',
-            data: json,
-            success: success,
-            error: error
-        });
-    };
-
-    var handler = function (parameters) {
-        parameters.delegate.getInstance = self.getInstance;
-        parameters.delegate.submit = self.submit;
-        var createHandler = new MvcModal.Entity.Create.Handler(parameters);
-        createHandler.init();
     };
 
     return {
-        Handler: handler
+        Initialize: function(parameters) {
+            parameters.delegate.instance = getInstance;
+            var handler = new MvcModal.Entity.Create(parameters);
+            handler.Initialize();
+        }
     };
 })();
--- Sources/MvcModal/Scripts/Entity.Create.js
+++ Sources/MvcModal/Scripts/Entity.Create.js
@@ -1,106 +1,101 @@
 ﻿MvcModal = window.MvcModal || {};
+MvcModal.Entity = MvcModal.Entity || {};
 
-MvcModal.Entity = MvcModal.Entity || (function() {
-    return {};
-})();
+MvcModal.Entity.Create = function (parameters) {
+    var params = parameters;
 
-MvcModal.Entity.Create = MvcModal.Entity.Create || (function () {
-    var dialog = function (parameters) {
-        var self = this;
-        var params = parameters;
-        this.getInstance = params.delegate.getInstance;
-        this.submit = params.delegate.submit;
-
-        this.onLoad = function (e) {
-            $(params.button.submit).on('click', self.onSubmitClicked);
-            $(params.button.cancel).on('click', self.onCancel);
-        };
-
-        this.onSubmitCompleted = function(data, status, xhr) {
-            if (!data.success) {
-                $(params.dialog.id).html(data.html);
-                self.onLoad();
-            } else {
-                $(params.dialog.updateId).html(data.html);
-                $(params.dialog.id).dialog('close');
-            }
-        };
-
-        this.onSubmitError = function(req) {
-
-        };
-        
-
-        this.onSubmitClicked = function(e) {
+    this.Initialize = function () {
+        $(params.button.open).on('click', function (e) {
             e.preventDefault();
 
-            var instance = self.getInstance(params);
-
-            self.submit({
-                url: params.dialog.url,
-                instance: instance,
-                success: self.onSubmitCompleted,
-                error: self.onSubmitError
-            });
+            var modal = new MvcModal.Entity.Create.Dialog(params);
+            modal.show();
 
             return false;
-        };
+        });
+    };
+};
+    
+MvcModal.Entity.Create.Dialog = function (parameters) {
+    var params = parameters;
+    var instance = params.delegate.instance;
+    var submit = params.delegate.submit || MvcModal.Entity.Create.Dialog.Submit;
 
-        this.onCancel = function (e) {
-            e.preventDefault();
+    this.show = function () {
+        $(params.dialog.id).dialog({
+            title: 'Create Album',
+            autoOpen: false,
+            resizable: true,
+            modal: true,
+            draggable: false,
+            open: onOpen,
+            close: onClose
+        });
+
+        $(params.dialog.id).dialog('open');
+    };
+
+    var onLoad = function (e) {
+        $(params.button.submit).on('click', onSubmit);
+        $(params.button.cancel).on('click', onCancel);
+    };
+
+    var onSubmit = function (e) {
+        e.preventDefault();
+
+        submit({
+            url: params.dialog.url,
+            instance: instance(params),
+            success: onSuccess,
+            error: onError
+        });
+
+        return false;
+    };
+
+    var onCancel = function (e) {
+        e.preventDefault();
+        $(params.dialog.id).dialog('close');
+        return false;
+    };
+
+    var onSuccess = function (data, status, xhr) {
+        if (!data.success) {
+            $(params.dialog.id).html(data.html);
+            onLoad();
+        } else {
+            $(params.dialog.updateId).html(data.html);
             $(params.dialog.id).dialog('close');
-            return false;
-        };
-
-        this.onOpen = function (event, ui) {
-            $(params.dialog.id).load(params.dialog.url, self.onLoad);
-        };
-
-        this.onClose = function(event, ui) {
-            $(params.button.submit).off('click');
-            $(params.button.cancel).off('click');
-        };
-
-        var show = function () {
-            $(params.dialog.id).dialog({
-                title: 'Create Album',
-                autoOpen: false,
-                resizable: true,
-                modal: true,
-                draggable: false,
-                open: self.onOpen,
-                close: self.onClose
-            });
-
-            $(params.dialog.id).dialog('open');
-        };
-
-        return {
-            show: show
-        };
+        }
     };
 
-    var handler = function (parameters) {
-        var params = parameters;
-        var modal = null;
+    var onError = function (req) {
 
-        var init = function () {
-            $(params.button.create).on('click', function (e) {
-                e.preventDefault();
-
-                modal = new dialog(params);
-                modal.show();
-
-                return false;
-            });
-        };
-        
-        return {
-            init: init
-        };
     };
 
-    return {
-        Handler: handler
+    var onOpen = function (event, ui) {
+        $(params.dialog.id).load(params.dialog.url, onLoad);
     };
-})();
+
+    var onClose = function (event, ui) {
+        $(params.button.submit).off('click');
+        $(params.button.cancel).off('click');
+    };
+};
+
+MvcModal.Entity.Create.Dialog.Submit = function(parameters) {
+    var url = parameters.url;
+    var instance = parameters.instance;
+    var success = parameters.success;
+    var error = parameters.error;
+    var json = JSON.stringify(instance);
+    $.ajax({
+        type: 'POST',
+        url: url,
+        datatype: 'json',
+        contentType: 'application/json: charset=utf-8',
+        data: json,
+        success: success,
+        error: error
+    });
+};
